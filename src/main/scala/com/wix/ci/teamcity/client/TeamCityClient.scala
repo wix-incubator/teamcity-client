8e1cf9dbe4af39bc792f074d3d074311c815f21e
eliorh <eliorh@wix.com>
2019-05-20 12:48:52 +0300

    new requests

--- src/main/scala/com/wix/ci/teamcity/client/TeamCityClient.scala
+++ src/main/scala/com/wix/ci/teamcity/client/TeamCityClient.scala
@@ -3,60 +3,81 @@ package com.wix.ci.teamcity.client
 import com.fasterxml.jackson.databind.{DeserializationFeature, ObjectMapper}
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 
-class TeamCityClient(httpClient : HttpClient, baseUrl : String) {
+class TeamCityClient(httpClient: HttpClient, baseUrl: String) {
   val mapper = MapperFactory.createMapper()
 
 
-  def createProject(project : BaseProject) : BaseProject = {
+  def createProject(project: BaseProject): BaseProject = {
     val url = s"${baseUrl}/${TeamCityClient.contextPrefix}/projects"
     val projectJson = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(project)
-    val json = httpClient.executePost(url,projectJson)
-    mapper.readValue(json,classOf[BaseProject])
+    val json = httpClient.executePost(url, projectJson)
+    mapper.readValue(json, classOf[BaseProject])
   }
 
-  def getProjects : Projects = {
+  def getProjects: Projects = {
     val url = s"${baseUrl}/${TeamCityClient.contextPrefix}/projects"
     val json = httpClient.executeGet(url)
-    mapper.readValue(json,classOf[Projects])
+    mapper.readValue(json, classOf[Projects])
   }
 
-  def deleteProject(projectId : String) : Unit = {
+  def deleteProject(projectId: String): Unit = {
     val url = s"${baseUrl}/${TeamCityClient.contextPrefix}/projects/id:${projectId}"
     httpClient.executeDelete(url)
   }
 
-  def getProjectById(projectId : String) : Project = {
+  def getProjectById(projectId: String): Project = {
     val url = s"${baseUrl}/${TeamCityClient.contextPrefix}/projects/id:${projectId}"
     val json = httpClient.executeGet(url)
-    mapper.readValue(json,classOf[Project])
+    mapper.readValue(json, classOf[Project])
   }
 
-  def getBuildTypes() : BuildTypes = {
+  def setProjectName(projectId: String, newProjectName: String): Unit = {
+    val url = s"${baseUrl}/${TeamCityClient.contextPrefix}/projects/id:${projectId}/name"
+    httpClient.executePutPlainText(url, newProjectName)
+  }
+
+  def getBuildTypes(): BuildTypes = {
     val url = s"${baseUrl}/${TeamCityClient.contextPrefix}/buildTypes"
     val json = httpClient.executeGet(url)
-    mapper.readValue(json,classOf[BuildTypes])
+    mapper.readValue(json, classOf[BuildTypes])
   }
 
-  def getBuildTypesByVcsRootId(vcsRootId : String) : BuildTypes ={
+  def getBuildTypesByVcsRootId(vcsRootId: String): BuildTypes = {
     val url = s"${baseUrl}/${TeamCityClient.contextPrefix}/buildTypes?locator=vcsRoot:(id:${vcsRootId})"
     val json = httpClient.executeGet(url)
-    mapper.readValue(json,classOf[BuildTypes])
+    mapper.readValue(json, classOf[BuildTypes])
+  }
+
+  def createBuildType(baseBuildType: BaseBuildType, projectId: String): BaseBuildType = {
+    val url = s"${baseUrl}/${TeamCityClient.contextPrefix}/projects/id:${projectId}/buildTypes"
+    val json = httpClient.executePost(url, mapper.writerWithDefaultPrettyPrinter.writeValueAsString(baseBuildType))
+    mapper.readValue(json, classOf[BaseBuildType])
+  }
+
+  def getVcsRoot(): Seq[BaseVcsRoot] = {
+    val url = s"${baseUrl}/${TeamCityClient.contextPrefix}/vcs-roots"
+    val json = httpClient.executeGet(url)
+    mapper.readValue(json, classOf[VcsRoots]).vcsRoots
+  }
+
+  def getVcsRootById(vcsRootId: String): VcsRoot = {
+    val url = s"${baseUrl}/${TeamCityClient.contextPrefix}/vcs-roots/id:$vcsRootId"
+    val json = httpClient.executeGet(url)
+    mapper.readValue(json, classOf[VcsRoot])
   }
 
 
-
-
-  def getBaseUrl() : String = baseUrl
+  def getBaseUrl(): String = baseUrl
 
 }
 
 
-object TeamCityClient{
+object TeamCityClient {
   val contextPrefix = "httpAuth/app/rest"
 }
 
-object MapperFactory{
-  def createMapper() : ObjectMapper = {
+object MapperFactory {
+  def createMapper(): ObjectMapper = {
     val objectMapper = new ObjectMapper
     objectMapper.registerModule(DefaultScalaModule)
     objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
--- src/test/scala/com/wix/ci/teamcity/client/TeamCityClientTest.scala
+++ src/test/scala/com/wix/ci/teamcity/client/TeamCityClientTest.scala
@@ -1,46 +1,86 @@
 package com.wix.ci.teamcity.client
 
 import com.wix.ci.teamcity.client.scalajhttp.HttpClientWrapper
-import org.specs2.mutable.SpecificationWithJUnit
 import org.specs2.matcher.{MustThrownExpectations, Scope}
 import org.specs2.mock.Mockito
+import org.specs2.mutable.SpecificationWithJUnit
 
-class TeamCityClientTest  extends SpecificationWithJUnit with Mockito{
-  "create project" should{
-    "call execute post and pass base project in body" in new Context{
+class TeamCityClientTest extends SpecificationWithJUnit with Mockito {
+  "create project" should {
+    "call execute post and pass base project in body" in new Context {
       teamcityClient.createProject(baseProject)
-      there was one(httpClient).executePost(createProjectUrl,writeObjectAsJson(baseProject))
+      there was one(httpClient).executePost(createProjectUrl, writeObjectAsJson(baseProject))
     }
   }
 
-  "delete project" should{
-    "call delete endpint with project id" in new Context{
+  "delete project" should {
+    "call delete endpoint with project id" in new Context {
       teamcityClient.deleteProject(baseProject.id)
       there was one(httpClient).executeDelete(deleteProjectUrl)
     }
   }
 
-  "get projects" should{
-    "return a list of all base projects" in new Context{
+  "get projects" should {
+    "return a list of all base projects" in new Context {
       teamcityClient.getProjects must beEqualTo(projects)
     }
   }
 
-  "get project by id" should{
-    "return the project " in new  Context{
+  "get project by id" should {
+    "return the project" in new Context {
       teamcityClient.getProjectById(projectId) must beEqualTo(project)
     }
   }
 
+  "set project name" should {
+    "call set project name endpoint" in new Context {
+      teamcityClient.setProjectName(projectId, projectName)
+      there was one(httpClient).executePutPlainText(setProjectName, projectName)
+    }
+  }
 
+  "get build types" should {
+    "return a list of build types " in new Context {
+      teamcityClient.getBuildTypes must beEqualTo(buildTypes)
+    }
+  }
 
+  "get build types by vcs root id" should {
+    "return a list of build types" in new Context {
+      teamcityClient.getBuildTypesByVcsRootId(vcsRootId) must beEqualTo(buildTypes)
+    }
+  }
 
+  "create build type" should {
+    "return base build type" in new Context {
+      teamcityClient.createBuildType(baseBuildTypes, projectId) must beEqualTo(baseBuildTypes)
+    }
+  }
 
+  "get vcs roots" should {
+    "return a list of vcs roots" in new Context {
+      teamcityClient.getVcsRoot() must beEqualTo(Seq(baseVcsRoot))
+    }
+  }
 
+  "get vcs root by id" should {
+    "return vcs root" in new Context {
+      teamcityClient.getVcsRootById(vcsRootId) must beEqualTo(vcsRoot)
+    }
+  }
 
-
-
-
+//  "get vcs root by name" should {
+//    "return vcs root" in new Context {
+//      teamcityClient.getVcsRoot() must beEqualTo(Seq(baseVcsRoot))
+//    }
+//  }
+//
+//  "get vcs root by vcs url" should {
+//    "return vcs root" in new Context {
+//      teamcityClient.getVcsRoot() must beEqualTo(Seq(baseVcsRoot))
+//    }
+//  }
+}
 
 
 //  "xxx" should{
@@ -52,31 +92,51 @@ class TeamCityClientTest  extends SpecificationWithJUnit with Mockito{
 //    }
 //  }
 
-  trait Context extends Scope with Mockito with MustThrownExpectations{
+trait Context extends Scope with Mockito with MustThrownExpectations {
 
-    val httpClient = mock[HttpClientWrapper]
-    val baseUrl = "http://localhost:8888/my-teamcity"
-    val teamcityClient = new TeamCityClient(httpClient,baseUrl)
-    val projectId = "projid"
-    val projectName = "projName"
-    val baseProject = BaseProject(projectId, "projName","some-href","some-web-url",Some("desc"),false,Some("some-parent-proj"))
-    val mapper = MapperFactory.createMapper()
-    val projects = Projects(1,List(baseProject))
-    val project = Project(projectId,projectName,"parentProjId1","some-href","some-weburl",Projects(0,List()),baseProject,BuildTypes(0,List()),None)
+  val httpClient = mock[HttpClientWrapper]
+  val baseUrl = "http://localhost:8888/my-teamcity"
+  val teamcityClient = new TeamCityClient(httpClient, baseUrl)
+  val projectId = "projid"
+  val projectName = "projName"
+  val vcsRootId = "vcsRootId"
+  val vcsRootName = "vcsRootName"
+  val property = Property("propName", "value", true)
+  val properties = Properties(Seq(property))
+  val baseProject = BaseProject(projectId, "projName","some-href","some-web-url",Some("desc"),false,Some("some-parent-proj"))
+  val mapper = MapperFactory.createMapper()
+  val buildTypes = BuildTypes(0, List())
+  val baseTemplate = BaseTemplate("tempId", "tempName", "href", projectName, projectId)
+  val baseBuildTypes = BaseBuildType("buildId", "buildName", "desc", baseTemplate, projectName, projectId)
+  val projects = Projects(1, List(baseProject))
+  val project = Project(projectId, projectName, "parentProjId1", "some-href", "some-weburl", Projects(0, List()), baseProject, buildTypes, None)
+  val baseVcsRoot = BaseVcsRoot(vcsRootId, vcsRootName, "href")
+  val vcsRoot = VcsRoot(vcsRootName, "status", "lastChecked", baseProject, properties)
+  val vcsRoots = VcsRoots(1, Seq(baseVcsRoot))
 
-    val createProjectUrl  = s"${baseUrl}/${TeamCityClient.contextPrefix}/projects"
-    val getProjectsUrl    = s"${baseUrl}/${TeamCityClient.contextPrefix}/projects"
-    val getProjectByIdUrl = s"${baseUrl}/${TeamCityClient.contextPrefix}/projects/id:${baseProject.id}"
-    val deleteProjectUrl  = s"${baseUrl}/${TeamCityClient.contextPrefix}/projects/id:${baseProject.id}"
+  val createProjectUrl = s"${baseUrl}/${TeamCityClient.contextPrefix}/projects"
+  val getProjectsUrl = s"${baseUrl}/${TeamCityClient.contextPrefix}/projects"
+  val setProjectName = s"${baseUrl}/${TeamCityClient.contextPrefix}/projects/id:${baseProject.id}/name"
+  val getProjectByIdUrl = s"${baseUrl}/${TeamCityClient.contextPrefix}/projects/id:${baseProject.id}"
+  val deleteProjectUrl = s"${baseUrl}/${TeamCityClient.contextPrefix}/projects/id:${baseProject.id}"
+  val createBuildTypeUrl = s"${baseUrl}/${TeamCityClient.contextPrefix}/projects/id:${baseProject.id}/buildTypes"
+  val getBuildTypes = s"${baseUrl}/${TeamCityClient.contextPrefix}/buildTypes"
+  val getBuildTypesByRootId = s"${baseUrl}/${TeamCityClient.contextPrefix}/buildTypes?locator=vcsRoot:(id:${vcsRootId})"
+  val getVcsRootsUrl = s"${baseUrl}/${TeamCityClient.contextPrefix}/vcs-roots"
+  val getVcsRootsByIdUrl = s"${baseUrl}/${TeamCityClient.contextPrefix}/vcs-roots/id:$vcsRootId"
 
 
-    httpClient.executePost(createProjectUrl,writeObjectAsJson(baseProject) )returns writeObjectAsJson(baseProject)
-    httpClient.executeGet(getProjectsUrl) returns writeObjectAsJson(projects)
-    httpClient.executeGet(getProjectByIdUrl) returns writeObjectAsJson(project)
+  httpClient.executePost(createProjectUrl, writeObjectAsJson(baseProject)) returns writeObjectAsJson(baseProject)
+  httpClient.executeGet(getProjectsUrl) returns writeObjectAsJson(projects)
+  httpClient.executeGet(getProjectByIdUrl) returns writeObjectAsJson(project)
+  httpClient.executeGet(getBuildTypes) returns writeObjectAsJson(buildTypes)
+  httpClient.executeGet(getBuildTypesByRootId) returns writeObjectAsJson(buildTypes)
+  httpClient.executePost(createBuildTypeUrl, writeObjectAsJson(baseBuildTypes)) returns writeObjectAsJson(baseBuildTypes)
+  httpClient.executeGet(getVcsRootsUrl) returns writeObjectAsJson(vcsRoots)
+  httpClient.executeGet(getVcsRootsByIdUrl) returns writeObjectAsJson(vcsRoot)
 
-    def writeObjectAsJson(obj : AnyRef) : String = {
-      mapper.writerWithDefaultPrettyPrinter().writeValueAsString(obj)
-    }
+  def writeObjectAsJson(obj: AnyRef): String = {
+    mapper.writerWithDefaultPrettyPrinter().writeValueAsString(obj)
   }
 }
 
