0de1c5a4cfc3a028cc07e46564cd9a70c55384a9
eliorh <eliorh@wix.com>
2019-05-20 12:47:15 +0300

    add new case classes

--- src/main/scala/com/wix/ci/teamcity/client/Domain.scala
+++ src/main/scala/com/wix/ci/teamcity/client/Domain.scala
@@ -1,25 +1,41 @@
 package com.wix.ci.teamcity.client
 
-case class Property(name : String, value : String, inherited : Boolean)
-case class BaseTemplate(id : String, name : String, href : String, projectName : String, projectId : String)
-case class BaseProject(id : String, name : String, href : String, webUrl : String, description : Option[String],archived : Boolean,parentProjectId : Option[String])
-case class Projects(count : Int, project : List[BaseProject])
-case class BaseBuildType(id : String,
-                         name : String,
-                         description : String,
-                         template : BaseTemplate,
-                         projectName : String,
-                         projectId : String)
-case class BuildTypes(count : Int, buildType : List[BaseBuildType])
-case class Parameters(count : Int, href : String, property : List[Property])
-case class Template(id : String, name : String, href : String, projectId : String, ProjectName : String, inherited : Boolean, templateFlag : Boolean)
+case class Property(name: String, value: String, inherited: Boolean)
+
+case class Properties(properties: Seq[Property])
+
+case class BaseTemplate(id: String, name: String, href: String, projectName: String, projectId: String)
+
+case class BaseProject(id: String, name: String, href: String, webUrl: String, description: Option[String], archived: Boolean, parentProjectId: Option[String])
+
+case class Projects(count: Int, project: List[BaseProject])
+
+case class BaseBuildType(id: String,
+                         name: String,
+                         description: String,
+                         template: BaseTemplate,
+                         projectName: String,
+                         projectId: String)
+
+case class BuildTypes(count: Int, buildType: List[BaseBuildType])
+
+case class Parameters(count: Int, href: String, property: List[Property])
+
+case class Template(id: String, name: String, href: String, projectId: String, ProjectName: String, inherited: Boolean, templateFlag: Boolean)
 
 case class Project(id: String,
                    name: String,
-                   parentProjectId : String,
-                   href : String,
-                   webUrl : String,
-                   projects : Projects,
-                   parentProject : BaseProject,
-                   buildTypes : BuildTypes,
-                   defaultTemplate : Option[Template] = None)
+                   parentProjectId: String,
+                   href: String,
+                   webUrl: String,
+                   projects: Projects,
+                   parentProject: BaseProject,
+                   buildTypes: BuildTypes,
+                   defaultTemplate: Option[Template] = None)
+
+
+case class BaseVcsRoot(id: String, name: String, href: String)
+
+case class VcsRoot(vcsName: String, status: String, lastChecked: String, project: BaseProject, properties: Properties)
+
+case class VcsRoots(count: Int, vcsRoots: Seq[BaseVcsRoot])
